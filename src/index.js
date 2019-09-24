4b7bf1bba3fb5f206db031b03aa68490fc674b44
Richard Ashkettle <richard.ashkettle@gmail.com>
2019-09-23 20:14:30 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line spaced-comment
+/// <reference path="../typings/phaser.d.ts" />
+
+import Phaser from 'phaser';
+import config from './config';
+import BootScene from './scenes/BootScene';
+import PreLoaderScene from './scenes/PreLoaderScene';
+import GameScene from './scenes/GameScene';
+
+
+class Game extends Phaser.Game {
+    constructor() {
+        super(config);
+        this.scene.add('BootScene', BootScene);
+        this.scene.add('PreLoaderScene', PreLoaderScene);
+        this.scene.add('Game', GameScene);
+        this.scene.start('BootScene');
+    }
+}
+
+function startGame() {
+    window.game = new Game();
+}
+
+window.onload = startGame();
---
+++ src/scenes/PreLoaderScene.js
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+import imglogo from '../assets/logo.png';
+
+
+export default class PreLoaderScene extends Phaser.Scene {
+    constructor() {
+        super('PreLoaderScene');
+    }
+
+    preload() {
+        this.load.image('logo', imglogo);
+    }
+
+    create() {
+        this.scene.start('Game');
+    }
+}
