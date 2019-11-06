6d96b379b6077a8948cb96e9b20209af3f7394c1
Richard Ashkettle <richard.ashkettle@gmail.com>
2019-11-06 14:57:29 -0500

    basic cleanup

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,7 +2,6 @@ import Phaser from 'phaser';
 import config from '../config';
 import Player from '../player';
 
-
 const gamestart = true;
 let player;
 
@@ -12,22 +11,16 @@ export default class GameScene extends Phaser.Scene {
     }
 
     createParallaxBackground() {
-        // create an tiled sprite with the size of our game screen
         this.bg_1 = this.add.tileSprite(0, 0, config.width, config.height, 'bg1');
-        // Set its pivot to the top left corner
         this.bg_1.setOrigin(0, 0);
-        // fix it so it won't move when the camera moves.
-        // Instead we are moving its texture on the update
         this.bg_1.setScrollFactor(0);
 
-        // Add a second background layer. Repeat as in bg_1
         this.bg_2 = this.add.tileSprite(0, 0, config.width, config.height, 'bg2');
         this.bg_2.setOrigin(0, 0);
         this.bg_2.setScrollFactor(0);
     }
 
     scrollParallaxBackground() {
-        // scroll the texture of the tilesprites proportionally to the layer
         this.bg_1.tilePositionX += 0.3;
         this.bg_2.tilePositionX += 0.6;
     }
